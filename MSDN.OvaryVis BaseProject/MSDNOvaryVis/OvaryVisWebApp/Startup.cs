699368461aef204d1edccab2790226fedfc1118e
Will Stott <will.stott@fitterwork.org>
2018-10-30 20:18:25 +0530

    implemented and tested MSDN.OvaryVis BaseProject

--- MSDN.OvaryVis BaseProject/MSDNOvaryVis/OvaryVisWebApp/Startup.cs
+++ MSDN.OvaryVis BaseProject/MSDNOvaryVis/OvaryVisWebApp/Startup.cs
@@ -32,7 +32,6 @@ namespace OvaryVisWebApp
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
